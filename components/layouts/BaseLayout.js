2064d464346c5b0168948e08d1fdc0cf6d19ecc6
skevprog <kevin-s93@hotmail.com>
2020-09-08 22:37:09 -0300

    feat: add static files, create home page

--- components/layouts/BaseLayout.js
+++ components/layouts/BaseLayout.js
@@ -1,10 +1,16 @@
 import Header from '../shared/Header'
 
-function BaseLayout({ children }) {
-  return (<>
-    <Header/>
-    {children}
-  </>)
+function BaseLayout({ children, className }) {
+  return (
+    <div className="layout-container">
+      <Header />
+      <main className={`cover ${className}`}>
+        <div className="wrapper">
+          {children}
+        </div>
+      </main>
+    </div>
+  )
 }
 
 export default BaseLayout
